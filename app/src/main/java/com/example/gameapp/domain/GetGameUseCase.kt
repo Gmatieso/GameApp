ae9f69694cf2d40af69857222465831ef260fc99
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-28 16:11:32 +0300

    Retriev list of GameItem objects from a datasource and shuffle their order

--- app/src/main/java/com/example/gameapp/domain/GetGameUseCase.kt
+++ app/src/main/java/com/example/gameapp/domain/GetGameUseCase.kt
@@ -1,7 +1,13 @@
 package com.example.gameapp.domain
 
+import com.example.gameapp.domain.items.GameItem
 import com.example.gameapp.repo.GameRepository
 import javax.inject.Inject
 
 class GetGameUseCase @Inject constructor(private val gameRepository: GameRepository){
+
+    //Defines usecase for retrieving a list of GameItem objects from a datasource and shuffle their order
+    suspend operator fun invoke(): List<GameItem> {
+        return gameRepository.getGames().shuffled()
+    }
 }
