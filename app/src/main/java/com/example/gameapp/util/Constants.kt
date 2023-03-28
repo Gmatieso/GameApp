cb2e78368cbc5f0347577fbfdfb3782c2706f59f
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-28 13:02:16 +0300

    Games Base URL and Endpoints

--- app/src/main/java/com/example/gameapp/util/Constants.kt
+++ app/src/main/java/com/example/gameapp/util/Constants.kt
@@ -1,4 +1,12 @@
 package com.example.gameapp.util
 
 class Constants {
+    companion object {
+
+        const val BASE_URL = "https://www.freetogame.com/api/"
+        const val GAMES_ENDPOINT = "games"
+        const val GAME_ID_ENDPOINT = "game?id="
+
+        const val KEY_GAME_ID = "com.example.gameapp.id"
+    }
 }
