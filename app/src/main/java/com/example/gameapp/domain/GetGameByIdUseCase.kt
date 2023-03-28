2d6021652c11416d89e44b4ecea0fabaeb0d30e4
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-28 16:17:55 +0300

    GetGameByIdUseCase Done

--- app/src/main/java/com/example/gameapp/domain/GetGameByIdUseCase.kt
+++ app/src/main/java/com/example/gameapp/domain/GetGameByIdUseCase.kt
@@ -1,4 +1,11 @@
 package com.example.gameapp.domain
 
-class GetGameByIdUseCase {
+import com.example.gameapp.domain.items.SpecificGameItem
+import com.example.gameapp.repo.GameRepository
+import javax.inject.Inject
+
+class GetGameByIdUseCase @Inject constructor(private val gameRepository: GameRepository){
+    suspend operator fun invoke(id: Int): SpecificGameItem {
+        return  gameRepository.getGameById(id)
+    }
 }
