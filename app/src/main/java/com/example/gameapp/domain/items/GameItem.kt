1f4df0bf05cb9bbc2f8817794234f7c589f24062
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-28 15:28:18 +0300

    GameModel to GameItem

--- app/src/main/java/com/example/gameapp/domain/items/GameItem.kt
+++ app/src/main/java/com/example/gameapp/domain/items/GameItem.kt
@@ -1,3 +1,12 @@
 package com.example.gameapp.domain.items
 
-data class GameItem()
+import com.example.gameapp.data.remote.models.GameModel
+
+data class GameItem(
+    val id: Int,
+    val title: String,
+    val thumbnail: String,
+    val short_description: String
+)
+
+fun GameModel.toGameItem() = GameItem(id, title, thumbnail, short_description)
