12d478c9a502aad651e52d9a9b8b77b165cac19a
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-28 13:30:29 +0300

    GameModel Refactored Done

--- app/src/main/java/com/example/gameapp/data/remote/models/GameModel.kt
+++ app/src/main/java/com/example/gameapp/data/remote/models/GameModel.kt
@@ -1,15 +1,9 @@
 package com.example.gameapp.data.remote.models
 
 data class GameModel(
-    val developer: String,
-    val freetogame_profile_url: String,
-    val game_url: String,
-    val genre: String,
     val id: Int,
-    val platform: String,
-    val publisher: String,
-    val release_date: String,
-    val short_description: String,
+    val title: String,
     val thumbnail: String,
-    val title: String
+    val short_description: String
+
 )
