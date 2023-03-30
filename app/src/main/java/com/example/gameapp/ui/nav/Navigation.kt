de4375681451f80c36a032a40032ee98fb65bd09
Gmatieso <matiesogeoffrey@gmail.com>
2023-03-30 15:51:58 +0300

    Navigation Screen Done

--- app/src/main/java/com/example/gameapp/ui/nav/Navigation.kt
+++ app/src/main/java/com/example/gameapp/ui/nav/Navigation.kt
@@ -1,2 +1,9 @@
 package com.example.gameapp.ui.nav
 
+import com.example.gameapp.util.Constants.Screens.DETAIL_SCREEN
+import com.example.gameapp.util.Constants.Screens.HOME_SCREEN
+
+sealed class Screens (val route: String) {
+    object  Home: Screens(route = HOME_SCREEN)
+    object  Details: Screens(route = DETAIL_SCREEN)
+}
